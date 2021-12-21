8a21283f241fac4095c945f596a97caf4f0a2968
Muhammad Numan <muhammadnuman70@gmail.com>
2021-12-21 14:33:00 +0500

    chore: release 0.2.0

--- lib/typescript/src/utils/index.d.ts
+++ lib/typescript/src/utils/index.d.ts
@@ -1,14 +1,10 @@
 export declare const normalize: (height: number) => number;
-export declare const getOverDragBoundries: (
-  snapPoints: any
-) => {
+export declare const getOverDragBoundries: (snapPoints: any) => {
   top: number;
   bounce: number;
 };
 export declare const getNormalizeSnaps: (snapPoints: any) => any;
 export declare const getSnapPoints: (snapPoints: any) => any;
-export declare const getInitialPosition: (
-  snapPoint: any
-) => {
+export declare const getInitialPosition: (snapPoint: any) => {
   y: number;
 };
